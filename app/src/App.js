5ae5c8922f1732b34b04c13aabba2a6a8eb390fb
Kirill Neevin <neevin-kirill@mail.ru>
2020-12-30 13:44:31 +0600

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import View from '@vkontakte/vkui/dist/components/View/View';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Home from './panels/Home';
+import Persik from './panels/Persik';
+
+const App = () => {
+	const [activePanel, setActivePanel] = useState('home');
+	const [fetchedUser, setUser] = useState(null);
+	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await bridge.send('VKWebAppGetUserInfo');
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
+
+	const go = e => {
+		setActivePanel(e.currentTarget.dataset.to);
+	};
+
+	return (
+		<View activePanel={activePanel} popout={popout}>
+			<Home id='home' fetchedUser={fetchedUser} go={go} />
+			<Persik id='persik' go={go} />
+		</View>
+	);
+}
+
+export default App;
+
---
+++ app/src/panels/Home.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+
+const Home = ({ id, go, fetchedUser }) => (
+	<Panel id={id}>
+		<PanelHeader>Example</PanelHeader>
+		{fetchedUser &&
+		<Group title="User Data Fetched with VK Bridge">
+			<Cell
+				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+			>
+				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+			</Cell>
+		</Group>}
+
+		<Group title="Navigation Example">
+			<Div>
+				<Button size="xl" level="2" onClick={go} data-to="persik">
+					Show me the Persik, please
+				</Button>
+			</Div>
+		</Group>
+	</Panel>
+);
+
+Home.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Home;
